93e725369985012be0378911a5357629cdb26cae
Adam Preston <adam.m.preston@gmail.com>
2018-07-02 15:41:50 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'NTD Data Importer for the Joint Application Package';
 }
